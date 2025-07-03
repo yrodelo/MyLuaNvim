af514ecc597f3ee7dd1aa4425d8886df4548c7e4
yrodelo <yrodelo@nequi.com>
2025-07-03 18:17:21 -0500

    🐛 resolve some bugs

--- lua/yesidev/plugins/nvim-treesitter.lua
+++ lua/yesidev/plugins/nvim-treesitter.lua
@@ -110,13 +110,13 @@ return {
 				auto_install = true, -- enable nvim-ts-rainbow plugin
 
 				rainbow = {
-					enable = true,
+					enable = false,
 					-- list of languages you want to disable the plugin for
 					disable = { "jsx", "cpp", "html" },
 					-- Which query to use for finding delimiters
-					query = "rainbow-parens",
+					-- query = "rainbow-parens",
 					-- Highlight the entire buffer all at once
-					strategy = require("ts-rainbow").strategy.global,
+					-- strategy = require("ts-rainbow").strategy.global,
 				},
 			})
 		end,
