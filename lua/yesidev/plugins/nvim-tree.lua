acdfcc92b36dca99339b6dd5316673cd46d371c7
yrodelo <yesiddevelope@gmail.com>
2026-06-24 12:57:08 -0500

    ✨ use nvim tree as terminal file explorer

--- lua/yesidev/plugins/nvim-tree.lua
+++ lua/yesidev/plugins/nvim-tree.lua
@@ -31,6 +31,12 @@ return {
 		-- configure nvim-tree
 		nvimtree.setup({
 			on_attach = my_on_attach,
+			sync_root_with_cwd = true,
+			respect_buf_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = true,
+			},
 			view = {
 				width = 35,
 				relativenumber = true,
